99ba8493a890b2601e3cc7803c5291b1797c449b
Nathan <nathan@calibrae.com>
2023-11-08 19:10:51 +0000

    Added validation feedback

--- resources/js/Pages/Products/ProductEdit.vue
+++ resources/js/Pages/Products/ProductEdit.vue
@@ -1,7 +1,7 @@
 <template>
     <Head title="Edit product" />
     <MainLayout>
-        <h1>
+        <h1 class="">
             Edit product details
         </h1>
 
@@ -13,20 +13,46 @@
                             <div class="row mb-3">
                                 <label for="title-input" class="col-sm-2 col-form-label">Title</label>
                                 <div class="col-sm-10">
-                                    <input :disabled="networking" type="text" class="form-control" id="title-input" v-model="model.title">
+                                    <input 
+                                        v-model="model.title"
+                                        :disabled="networking"
+                                        :class="{'is-invalid': errors.title}" 
+                                        type="text" 
+                                        class="form-control" 
+                                        id="title-input">
+
+                                        <div v-for="error in errors.title" class="invalid-feedback" :key="error">
+                                            {{ error }}
+                                        </div>
                                 </div>
                             </div>
                             <div class="row mb-3">
                                 <label for="desc-input" class="col-sm-2 col-form-label">Description</label>
                                 <div class="col-sm-10">
-                                    <textarea :disabled="networking" rows="4" class="form-control" id="desc-input" v-model="model.description"></textarea>
+                                    <textarea 
+                                        v-model="model.description"
+                                        :class="{'is-invalid': errors.description}" 
+                                        :disabled="networking" 
+                                        rows="4" 
+                                        class="form-control" 
+                                        id="desc-input" />
+
+                                    <div v-for="error in errors.description" class="invalid-feedback" :key="error">
+                                        {{ error }}
+                                    </div>
                                 </div>
                             </div>
-                            <button :disabled="submitDisabled" type="submit" class="btn" :class="submitDisabled ? 'btn-outline-secondary' : 'btn-primary'">
-                                {{
-                                    networking ? 'Saving...' : 'Save edit'
-                                }}
-                            </button>
+                            <div class="row mt-4">
+                                <div class="offset-sm-2 col-sm-10">
+                                    <button :disabled="submitDisabled" type="submit" class="btn" :class="submitDisabled ? 'btn-outline-secondary' : 'btn-primary'">
+                                        {{
+                                            networking ? 'Saving...' : 'Save edit'
+                                        }}
+                                    </button>
+
+                                    <Link class="btn btn-link float-end" :href="route('products.show', product)">Cancel</Link>
+                                </div>
+                            </div>
                         </form>
 
                     </div>
@@ -57,7 +83,8 @@ export default {
             model: {...this.product},
             networking: false,
             dirty: false,
-            errors: {}
+            errors: this.resetErrors(),
+            errorMessage: ''
         }
     },
 
@@ -74,33 +101,41 @@ export default {
     watch: {
         'model.title': function() {
             this.setDirty(true);
+            delete this.errors['title'];
         },
 
         'model.description'() {
             this.setDirty(true);
+            delete this.errors['description'];
         }
     },
 
     methods: {
+
+        resetErrors() {
+            return {};
+        },
+
         setDirty(isDirty) {
             this.dirty = isDirty;
+            this.errorMessage = '';
         },
         
         save() {
             this.networking = true;
             axios.patch(route('api.products.update', this.model), this.model)
                 .then(this.onSaved)
-                .catch(this.onSaveError)
+                .catch(error => this.onSaveError(error))
         },
 
         onSaved() {
+            this.errors = this.resetErrors();
             this.onSaveComplete();
         },
 
         onSaveError(error) {
             this.errors = error.response.data.errors;
-            alert(error.response.data.message);
-
+            this.errorMessage = error.response.data.message;
             this.onSaveComplete();
         },
 
